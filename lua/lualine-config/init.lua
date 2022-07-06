704903fcf328b5146227dd2a0fdb189e031af273
luwanquan <1501474302@qq.com>
2022-07-07 01:03:03 +0800

    feat: 更换回lsp

--- lua/lualine-config/init.lua
+++ lua/lualine-config/init.lua
@@ -1,12 +1,12 @@
--- local gps = require("nvim-gps")
+local gps = require("nvim-gps")
 
--- local function gps_content()
---	if gps.is_available() then
---		return gps.get_location()
---	else
---		return ""
---	end
--- end
+local function gps_content()
+	if gps.is_available() then
+		return gps.get_location()
+	else
+		return ""
+	end
+end
 
 local mini_sections = {
 	lualine_a = {},
@@ -65,14 +65,82 @@ require("lualine").setup({
 		lualine_b = { { "branch" }, { "diff" } },
 		lualine_c = {
 			{ "lsp_progress" },
--- 			{ gps_content, cond = gps.is_available },
+			{ gps_content, cond = gps.is_available },
 		},
 		lualine_x = {
 			{
 				"diagnostics",
 				sources = { "nvim_diagnostic" },
-				symbols = { error = " ", warn = " ", info = " " },
+				symbols = { error = " ", warn = " ", info = " ", hint = " " },
 			},
+			{
+				-- Lsp server name .
+				function()
+					local msg = "No Active Lsp"
+					local buf_ft = vim.api.nvim_buf_get_option(0, "filetype")
+					local clients = vim.lsp.get_active_clients()
+					if next(clients) == nil then
+						return msg
+					end
+					for _, client in ipairs(clients) do
+						local filetypes = client.config.filetypes
+						-- print(client.name, filetypes[0])
+						if filetypes and vim.fn.index(filetypes, buf_ft) ~= -1 then
+							return client.name
+						end
+					end
+					return msg
+				end,
+				-- icon = "  LSP:",
+				color = { fg = "#ffffff" },
+			},
+			-- {
+			-- 	function()
+			-- 		local b = vim.api.nvim_get_current_buf()
+			-- 		if next(vim.treesitter.highlighter.active[b]) then
+			-- 			return ""
+			-- 		end
+			-- 		return ""
+			-- 	end,
+			-- 	-- color = { fg = colors.green },
+			-- 	-- cond = conditions.hide_in_width,
+			-- },
+			-- {
+			-- 	function(msg)
+			-- 		msg = msg or "LS Inactive"
+			-- 		local buf_clients = vim.lsp.buf_get_clients()
+			-- 		if next(buf_clients) == nil then
+			-- 			-- TODO: clean up this if statement
+			-- 			if type(msg) == "boolean" or #msg == 0 then
+			-- 				return "LS Inactive"
+			-- 			end
+			-- 			return msg
+			-- 		end
+			-- 		local buf_ft = vim.bo.filetype
+			-- 		local buf_client_names = {}
+			--
+			-- 		-- add client
+			-- 		for _, client in pairs(buf_clients) do
+			-- 			if client.name ~= "null-ls" then
+			-- 				table.insert(buf_client_names, client.name)
+			-- 			end
+			-- 		end
+			--
+			-- 		-- add formatter
+			-- 		local formatters = require("lvim.lsp.null-ls.formatters")
+			-- 		local supported_formatters = formatters.list_registered(buf_ft)
+			-- 		vim.list_extend(buf_client_names, supported_formatters)
+			--
+			-- 		-- add linter
+			-- 		local linters = require("lvim.lsp.null-ls.linters")
+			-- 		local supported_linters = linters.list_registered(buf_ft)
+			-- 		vim.list_extend(buf_client_names, supported_linters)
+			--
+			-- 		return "[" .. table.concat(buf_client_names, ", ") .. "]"
+			-- 	end,
+			-- 	color = { gui = "bold" },
+			-- 	-- cond = conditions.hide_in_width,
+			-- },
 		},
 		lualine_y = {
 			{
--- lua/packer-config/init.lua
+++ lua/packer-config/init.lua
@@ -70,35 +70,35 @@ return require("packer").startup(function()
 	use({ "iamcco/markdown-preview.nvim", opt = true, ft = "markdown", run = "cd app && yarn install" })
 
 	-- 代码解析树以及相关插件 treesitter plugins
-	-- use("nvim-treesitter/nvim-treesitter", { run = ":TSUpdate" })
-	-- use("nvim-treesitter/nvim-treesitter-textobjects")
-	-- use("nvim-treesitter/nvim-treesitter-refactor") -- 突出显示定义、跳转定义、定义重命名等
-	-- use("SmiteshP/nvim-gps")
-	-- use("p00f/nvim-ts-rainbow") -- 代码块括号标线
-	-- use({
-	-- 	"numToStr/Comment.nvim",
-	-- 	requires = {
-	-- 		"JoosepAlviste/nvim-ts-context-commentstring",
-	-- 	},
-	-- 	config = function()
-	-- 		require("comment-config")
-	-- 	end,
-	-- })
-	-- -- use("mfussenegger/nvim-ts-hint-textobject")
-	-- use({
-	-- 	"windwp/nvim-autopairs",
-	-- 	config = function()
-	-- 		require("nvim-autopairs-config")
-	-- 	end,
-	-- })
-	-- use({
-	-- 	"windwp/nvim-ts-autotag",
-	-- 	opt = true,
-	-- 	ft = { "html", "xml" },
-	-- 	config = function()
-	-- 		require("nvim-ts-autotag-config")
-	-- 	end,
-	-- })
+  use("nvim-treesitter/nvim-treesitter", { run = ":TSUpdate" })
+	use("nvim-treesitter/nvim-treesitter-textobjects")
+	use("nvim-treesitter/nvim-treesitter-refactor") -- 突出显示定义、跳转定义、定义重命名等
+	use("SmiteshP/nvim-gps")
+	use("p00f/nvim-ts-rainbow") -- 代码块括号标线
+	use({
+		"numToStr/Comment.nvim",
+		requires = {
+			"JoosepAlviste/nvim-ts-context-commentstring",
+		},
+		config = function()
+			require("comment-config")
+		end,
+	})
+	-- use("mfussenegger/nvim-ts-hint-textobject")
+	use({
+		"windwp/nvim-autopairs",
+		config = function()
+			require("nvim-autopairs-config")
+		end,
+	})
+	use({
+		"windwp/nvim-ts-autotag",
+		opt = true,
+		ft = { "html", "xml" },
+		config = function()
+			require("nvim-ts-autotag-config")
+		end,
+	})
 
 	-- 🔭望远镜 telescope plugins
 	use("nvim-telescope/telescope-fzf-native.nvim")
@@ -114,54 +114,54 @@ return require("packer").startup(function()
 	})
 
 	-- LSP 语言服务器
-	-- use("neovim/nvim-lspconfig") -- Colection of configurations for the built-in LSP client
-	-- use("williamboman/nvim-lsp-installer")
-	-- use({
-	-- 	"hrsh7th/nvim-cmp",
-	-- 	requires = {
-	-- 		{ "onsails/lspkind-nvim" }, -- 为补全添加类似 vscode 的图标
-	-- 		{ "hrsh7th/vim-vsnip" }, -- vsnip 引擎，用于获得代码片段支持
-	-- 		{ "hrsh7th/cmp-vsnip" }, -- 适用于 vsnip 的代码片段源
-	-- 		{ "hrsh7th/cmp-nvim-lsp" }, -- 替换内置 omnifunc，获得更多补全
-	-- 		{ "hrsh7th/cmp-path" }, -- 路径补全
-	-- 		{ "hrsh7th/cmp-buffer" }, -- 缓冲区补全
-	-- 		{ "hrsh7th/cmp-cmdline" }, -- 命令补全
-	-- 		{ "f3fora/cmp-spell" }, -- 拼写建议
-	-- 		{ "rafamadriz/friendly-snippets" }, -- 提供多种语言的代码片段
-	-- 		{ "lukas-reineke/cmp-under-comparator" }, -- 让补全结果的排序更加智能
-	-- 		{ "tzachar/cmp-tabnine", run = "./install.sh" }, -- tabnine 源,提供基于 AI 的智能补全
-	-- 	},
-	-- 	config = function()
-	-- 		require("lsp-config.nvim-cmp")
-	-- 	end,
-	-- }) -- 自动完成 Autocompletion plugin
-	-- use("hrsh7th/cmp-nvim-lsp") -- LSP source for nvim-cmp
-	-- use("saadparwaiz1/cmp_luasnip") -- lua 代码块自动完成 Snippets source for nvim-cmp
-	-- use("L3MON4D3/LuaSnip") -- lua 代码块 Snippets plugin
-	-- use({
-	-- 	"tami5/lspsaga.nvim",
-	-- 	config = function()
-	-- 		require("lsp-config.lspsaga")
-	-- 	end,
-	-- })
-	-- use("onsails/lspkind.nvim")
-	-- use({
-	-- 	"jose-elias-alvarez/null-ls.nvim",
-	-- 	config = function()
-	-- 		require("lsp-config.null-ls")
-	-- 	end,
-	-- }) --> inject lsp diagnistocs, formattings, code actions, and more ...
-	-- use("jose-elias-alvarez/typescript.nvim")
+	use("neovim/nvim-lspconfig") -- Colection of configurations for the built-in LSP client
+	use("williamboman/nvim-lsp-installer")
+	use({
+		"hrsh7th/nvim-cmp",
+		requires = {
+			{ "onsails/lspkind-nvim" }, -- 为补全添加类似 vscode 的图标
+			{ "hrsh7th/vim-vsnip" }, -- vsnip 引擎，用于获得代码片段支持
+			{ "hrsh7th/cmp-vsnip" }, -- 适用于 vsnip 的代码片段源
+			{ "hrsh7th/cmp-nvim-lsp" }, -- 替换内置 omnifunc，获得更多补全
+			{ "hrsh7th/cmp-path" }, -- 路径补全
+			{ "hrsh7th/cmp-buffer" }, -- 缓冲区补全
+			{ "hrsh7th/cmp-cmdline" }, -- 命令补全
+			{ "f3fora/cmp-spell" }, -- 拼写建议
+			{ "rafamadriz/friendly-snippets" }, -- 提供多种语言的代码片段
+			{ "lukas-reineke/cmp-under-comparator" }, -- 让补全结果的排序更加智能
+			{ "tzachar/cmp-tabnine", run = "./install.sh" }, -- tabnine 源,提供基于 AI 的智能补全
+		},
+		config = function()
+			require("lsp-config.nvim-cmp")
+		end,
+	}) -- 自动完成 Autocompletion plugin
+	use("hrsh7th/cmp-nvim-lsp") -- LSP source for nvim-cmp
+	use("saadparwaiz1/cmp_luasnip") -- lua 代码块自动完成 Snippets source for nvim-cmp
+	use("L3MON4D3/LuaSnip") -- lua 代码块 Snippets plugin
+	use({
+		"tami5/lspsaga.nvim",
+		config = function()
+			require("lsp-config.lspsaga")
+		end,
+	})
+	use("onsails/lspkind.nvim")
+	use({
+		"jose-elias-alvarez/null-ls.nvim",
+		config = function()
+			require("lsp-config.null-ls")
+		end,
+	}) --> inject lsp diagnistocs, formattings, code actions, and more ...
+	use("jose-elias-alvarez/typescript.nvim")
 
 	-- coc completion
-	use({
-		"neoclide/coc.nvim",
-		branch = "release",
-		config = function()
-			vim.api.nvim_exec([[source ~/.config/nvim/lua/coc-nvim-config/coc_config.vim]], false)
-			-- require("coc-nvim-config")
-		end,
-		rum = "yarn && yarn build",
-	})
+	-- use({
+	-- 	"neoclide/coc.nvim",
+	-- 	branch = "release",
+	-- 	config = function()
+	-- 		vim.api.nvim_exec([[source ~/.config/nvim/lua/coc-nvim-config/coc_config.vim]], false)
+	-- 		-- require("coc-nvim-config")
+	-- 	end,
+	-- 	rum = "yarn && yarn build",
+	-- })
 	-- use({ "github/copilot.vim", opt = true, cmd = "Copilot" })
 end)
